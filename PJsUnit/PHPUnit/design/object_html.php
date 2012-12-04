9238d25112c56994c1078c466498d218ff1362fa
FrejKnutar <frej@knutar.se>
2012-12-04 15:51:50 +0100

    Second Commit

---
+++ PJsUnit/PHPUnit/design/object_html.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+
+$string .= "<ol>";
+foreach($methods as $m) {
+	$string .= "<li>$m</li>";
+}
+$string .= "<p><label>Methods:</label>&nbsp".count($methods)."</p>";
+$string .= "<p><label>Passed:</label>&nbsp$passed_count (" . (count($methods)>0 ? ($passed_count/count($methods))*100 .'%' : 'NA').")</p>";
+$string .= "<p><label>Failed:</label>&nbsp$failed_count (" . (count($methods)>0 ? ($failed_count/count($methods))*100 .'%' : 'NA').")</p>";
+$string .= "</ol>";
+return $string;
+?>
